4439ce77c15bdc16bf7991a929ec563a1becc1a1
wiloon <wiloon.wy@gmail.com>
2021-02-18 22:34:55 +0800

    cmake

---
+++ CMakeFiles/Cx.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cx.dir/hello.c.o"
+  "Cx"
+  "Cx.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Cx.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
